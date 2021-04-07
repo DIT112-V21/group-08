1ac2b6e3f1d1f5d66c8f5c2ede6fd98ac18ff93e
Alnor985 <71012818+Alnor985@users.noreply.github.com>
2021-04-07 15:46:11 +0200

    Makes the car go and stop when sensor triggers
    The code necessary for the Basic Obstacle Avoidance assignment. We have written more advanced obstacle avoidance already, which is on the Advanced Obstacle Avoidance branch. Not sure if this needs to be merged into master for the completion of the assignment, but I need to provide a pull request to turn in.

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,27 @@
 #include <Smartcar.h>
+const int TRIGGER_PIN           = 6; 
+const int ECHO_PIN              = 7;
+const unsigned int MAX_DISTANCE = 1000;
+ArduinoRuntime arduinoRuntime;
+SR04 sensor(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+SimpleCar car(control);
+
+int const stop_dist = 200;
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  car.setSpeed(60);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(sensor.getDistance() > 0 && sensor.getDistance() < stop_dist){
+    car.setSpeed(0);
+  }
+ 
 }
