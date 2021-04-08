8493ef924e295afcdae0a5f3b7e09af4144d57d0
adamtelles <71008096+adamtelles@users.noreply.github.com>
2021-04-08 21:53:00 +0200

    Smarter decision making
    Upon reaching an obstacle the car will stop and check to the left and right to see which seems to be the better route.

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -49,70 +49,169 @@ void setup()
 
 void loop()
 {
-    //Serial.println(sideFrontIR.getDistance());
-    //Serial.println(front.getDistance());
-    delay(100);
-    handleInput();
-    obstacleAvoid();
 
-    //gyro.update();
-    //Serial.println(gyro.getHeading());
-    //Serial.println(rightOdometer.getSpeed());
-
+  obstacleAvoid();
     
 }
 
+// TODO
+/*
+ * Dont turn back to face forward when it should go right
+ */
 
 void obstacleAvoid(){
-
-    int distance = front.getDistance();
-
-    if(distance > 0 && distance < 125){ // Begin turning
-      //car.setSpeed(0);
-      //car.setAngle(0);
+  int distance = front.getDistance();
+  
+  if(distance > 0 && distance < 100){
+    // 0 left is free, 1 right is free
+    int checkResults = checkSides();
+    if(checkResults == 2){
+      gyro.update();
+      int startGyro = gyro.getHeading();
+      int gyroCurrent = startGyro;
       
-      //car.setSpeed(fSpeed);
-
-      if (rightIR.getDistance() > 0){
-        car.setSpeed(0.25 * fSpeed);
-        car.setAngle(lDegrees);
-      }
-      else if (leftIR.getDistance() > 0){
-        car.setSpeed(0.25 * fSpeed);
-        car.setAngle(rDegrees);
-      }
-      else {
-        car.setSpeed(0.25 * fSpeed);
-        car.setAngle(lDegrees);
-      }
-
-      delay(300);
-    }
-    else if(distance > 0 && distance < 220){ // slow down more
-        car.setSpeed(0.25 * fSpeed);
-        car.setAngle(0);
-    }
-    else if(distance > 0 && distance < 400){ // slow down
-        car.setSpeed(0.5 * fSpeed);
-        car.setAngle(0);
-    }
-    else if(distance == 0){
+      turnLeft(startGyro);
+      turnLeft(startGyro);
+      
       car.setSpeed(fSpeed);
       car.setAngle(0);
       delay(100);
+    }
+    else if(checkResults == 0) {
+      gyro.update();
+      turnLeft(gyro.getHeading());
+      car.setSpeed(fSpeed);
+      car.setAngle(0);
+      delay(100);
+    } else {
+      gyro.update();
+      turnRight(gyro.getHeading());
+      car.setSpeed(fSpeed);
+      car.setAngle(0);
+      delay(1000);
+    }
+  }
+  else if(distance > 0 && distance < 200){ // slow down more
+    car.setSpeed(0.3 * fSpeed);
+    car.setAngle(0);
+  }
+  else if(distance > 0 && distance < 300){ // slow down
+    car.setSpeed(0.5 * fSpeed);
+    car.setAngle(0);
+  }
+  
+  car.setSpeed(fSpeed);
+  car.setAngle(0);
+  delay(100);
+}
 
-      if(rightOdometer.getSpeed() == 0.0){
-          car.setSpeed(bSpeed);
-          car.setAngle(rDegrees);
-          delay(2000);
-      }
-      
+
+// Rotates 90 degrees left, then 180 right (90 degrees right from start)
+int checkSides() {
+    waitStop();
+    Serial.println("Checking sides");
+    gyro.update();
+    
+    int startGyro = gyro.getHeading();
+    int gyroCurrent = startGyro;
+    int distances [2] = {0,0}; 
+
+    turnLeft(startGyro);
+    distances[0] = front.getDistance();
+    
+    turnRight(startGyro);
+    distances[1] = front.getDistance();
+    
+    faceForward(startGyro);
+
+    Serial.println(distances[0]);
+    Serial.println(distances[1]);
+    
+    if((distances[0] < 100) && (distances[1] < 100) && (distances[0] > 0) && (distances[1] > 0)){
+      Serial.println("Returned 2");
+      return 2;
+    }
+    else if(distances[0] == 0){
+      Serial.println("Returned 0");
+      return 0;
+    }
+    else if((distances[0] > distances[1])){
+      Serial.println("Returned 0");
+      return 0;
+    } else {
+      Serial.println("Returned 1");
+      return 1;
     }
     
+}
+
+void turnLeft(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    const double SPEED_REDUCE = 0.6;
+
+    int gyroCurrent = startGyro;
+    
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * fSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * bSpeed);
+      gyro.update();
+
+      gyroCurrent = gyro.getHeading();
+      
+    } while(((gyroCurrent + 180) % 360 != (startGyro + 180 + 90) % 360 ));
+    
+      rightMotor.setSpeed(0);
+      leftMotor.setSpeed(0);
+}
+
+void turnRight(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    const double SPEED_REDUCE = 0.6;
 
 
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * bSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * fSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while((gyroCurrent + 180) % 360 != abs(startGyro + 180 - 90) % 360 );
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void faceForward(int startGyro){
+  gyro.update();
+  int gyroCurrent = startGyro;
+  
+  const double SPEED_REDUCE = 0.6;
+    do {
+      gyroCurrent = gyro.getHeading();
+      rightMotor.setSpeed(SPEED_REDUCE * fSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * bSpeed);
+      gyro.update();
+    } while(gyroCurrent != startGyro);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void waitStop(){
+  car.setSpeed(0);
+  rightMotor.setSpeed(0);
+  leftMotor.setSpeed(0);
+
+
+  while((leftOdometer.getSpeed() > 0) || (rightOdometer.getSpeed() > 0)){
+    delay(10); 
+  }
   
 }
+
 
 
 void handleInput()
@@ -123,21 +222,24 @@ void handleInput()
                                     // the last entry
         switch (input)
         {
-        case 'l': // rotate counter-clockwise going forward
+        case 'a': // rotate counter-clockwise going forward
             car.setSpeed(fSpeed);
             car.setAngle(lDegrees);
             break;
-        case 'r': // turn clock-wise
+        case 'd': // turn clock-wise
             car.setSpeed(fSpeed);
             car.setAngle(rDegrees);
             break;
-        case 'f': // go ahead
+        case 'w': // go ahead
             car.setSpeed(fSpeed);
             car.setAngle(0);
             break;
-        case 'b': // go back
+        case 's': // go back
             car.setSpeed(bSpeed);
             car.setAngle(0);
+            break;
+        case 'c': // go back
+            checkSides();
             break;
         default: // if you receive something that you don't know, just stop
             car.setSpeed(0);
