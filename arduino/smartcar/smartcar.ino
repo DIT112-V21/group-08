99d70b3ddd73553e3b1ed8f7ece41f6bbe4485bf
lnoklal <lnoklal@student.chalmers.se>
2021-05-04 15:51:02 +0200

    Added method that takes distance as parameter

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -6,8 +6,21 @@ BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
 const int GYROSCOPE_OFFSET = 0;
+const unsigned long PULSES_PER_METER = 1000;
 GY50 gyro(arduinoRuntime, GYROSCOPE_OFFSET);
-SimpleCar car(control);
+
+DirectionlessOdometer leftOdometer{
+  arduinoRuntime,
+  smartcarlib::pins::v2::leftOdometerPin,
+  []() { leftOdometer.update(); },
+  PULSES_PER_METER};
+DirectionlessOdometer rightOdometer{
+  arduinoRuntime,
+  smartcarlib::pins::v2::rightOdometerPin,
+  []() { rightOdometer.update(); },
+  PULSES_PER_METER};
+    
+SmartCar car(arduinoRuntime, control, gyro, leftOdometer, rightOdometer);
 
 
 void setup() {
@@ -16,7 +29,9 @@ void setup() {
 }
 
 void loop() {
-  turnTest();
+  //turnTest();
+  driveTest();
+  
 
 }
 
@@ -68,4 +83,24 @@ void turnRight(int turnDegrees){
     }  while((gyroCurrent + 180) % 360 != abs(startGyro + 180 - turnDegrees) % 360 );
     rightMotor.setSpeed(0);
     leftMotor.setSpeed(0);
+}
+
+void driveTest(){
+  long driveDist = 500; // For testing. This would be determined by input.
+  drive(driveDist);
+  delay(5000);
+}
+
+ //Drive is a method for taking a distance as a parameter and driving that distance. So far innacurate, due to rate of updating. The slower the car moves the more accurate it will be. Unsure of how to solve this drawback. 
+void drive(long driveDist){          
+  long startDist = car.getDistance();
+  long currentDist = startDist;
+  long goalDist = (startDist + driveDist);
+
+  do{
+    car.setSpeed(fSpeed);
+    currentDist = car.getDistance();
+  }while(currentDist <= goalDist);
+  car.setSpeed(0); 
+
 }
