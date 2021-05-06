57bf7a270e4eb40320663ab10f9e24778c941bb7
adamtelles <71008096+adamtelles@users.noreply.github.com>
2021-05-06 17:37:40 +0200

    Sketch with MQTT for Pathfinder iOS App

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -4,68 +4,40 @@
 
 MQTTClient mqtt;
 
+
 const int lDegrees = -90; // degrees to turn left
 const int rDegrees = 90;  // degrees to turn right
 int manualSpeed = 70;
 bool goingForward = true; //true is forward, false is backward
+const unsigned long LEFT_PULSES_PER_METER  = 600;
+const unsigned long RIGHT_PULSES_PER_METER = 740;
+const double SPEED_REDUCE = 0.3;
 
 ArduinoRuntime arduinoRuntime;
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
 
+DirectionalOdometer leftOdometer(
+    arduinoRuntime,
+    smartcarlib::pins::v2::leftOdometerPins,
+    []() { leftOdometer.update(); },
+    LEFT_PULSES_PER_METER);
+DirectionalOdometer rightOdometer(
+    arduinoRuntime,
+    smartcarlib::pins::v2::rightOdometerPins,
+    []() { rightOdometer.update(); },
+    RIGHT_PULSES_PER_METER);
+
+const int GYROSCOPE_OFFSET = 37;
+GY50 gyro(arduinoRuntime, GYROSCOPE_OFFSET);
 
 SimpleCar car(control);
 
-void setup() {
-    Serial.begin(9600);
-    //Uncomment line below
-    //mqtt.begin("localhost", 1883, WiFi);
-    // Will connect to localhost port 1883 be default
-    if (mqtt.connect("arduino", "public", "public")) {
-        mqtt.subscribe("/", 2); // Subscribing to topic "/"
-        mqtt.subscribe("speed", 2); //Subscribing to topic "speed"
-        mqtt.onMessage(+[](String topic, String message) {
-          Serial.println(message);
-            if(topic == "/"){
-              if(message == "w") {
-                car.setSpeed(manualSpeed);
-                car.setAngle(0);
-                Serial.println("XXX");
-              }
-              else if(message == "stop") {
-                car.setSpeed(0);
-                car.setAngle(0);
-              }
-              else if(message == "s") {
-                car.setSpeed(-manualSpeed);
-                car.setAngle(0);
-              }
-              else if(message == "d") {
-                car.setSpeed(manualSpeed);
-                car.setAngle(rDegrees);
-              }
-              else if(message == "a") {
-                car.setSpeed(manualSpeed);
-                car.setAngle(lDegrees);
-              }
-              else {
-                car.setSpeed(0);
-                car.setAngle(0);
-              }
-            }
-            if(topic == "speed"){
-              car.setSpeed(message.toInt());
-              car.setAngle(0);
-            }
-    }
-            
-            
-        );}
-}
 
 
-// Start server in terminal with: /usr/local/sbin/mosquitto
+// Instructions for starting and using mosquitto on macOS
+// Start server with terminal: /usr/local/sbin/mosquitto
 
 // Start sketch
 
@@ -76,10 +48,243 @@ void setup() {
 
 
 
+void setup() {
+    Serial.begin(9600);
+    // IMPORTANT: Uncomment line bellow (line 54) before compiling, causes error in CI for some reason but works perfectly fine in SMCE
+    //mqtt.begin("localhost", 1883, WiFi);
+    // Will connect to localhost port 1883 be default
+    if (mqtt.connect("arduino", "public", "public")) {
+        mqtt.subscribe("/", 2);    // Subscribing to topic "/"
+        mqtt.onMessage(+[](String topic, String message) {
+            // handle the message
+            Serial.println("Topic: " + topic + " Message: " + message);
+            if(message == "r0.0") {
+              gyro.update();
+              rotateTo0(gyro.getHeading());
+            }
+            else if(message == "r315.0") {
+              gyro.update();
+              rotateTo315(gyro.getHeading());
+            }
+            else if(message == "r270.0") {
+              gyro.update();
+              rotateTo270(gyro.getHeading());
+            }
+            else if(message == "r225.0") {
+              gyro.update();
+              rotateTo225(gyro.getHeading());
+            }
+            else if(message == "r180.0") {
+              gyro.update();
+              rotateTo180(gyro.getHeading());
+            }
+            else if(message == "r135.0") {
+              gyro.update();
+              rotateTo135(gyro.getHeading());
+            }
+            else if(message == "r90.0") {
+              gyro.update();
+              rotateTo90(gyro.getHeading());
+            }
+            else if(message == "r45.0") {
+              gyro.update();
+              rotateTo45(gyro.getHeading());
+            }
+            else if(message == "w") {
+              waitStop();
+            }
+            else { 
+              goForward(message.toDouble());
+            }
+            
+            
+        });
+    }
+}
+
+
 void loop() {
     if (mqtt.connected()) {
-        //mqtt.publish("/", "Hello");
-        //mqtt.publish("speed", manualSpeed);
+        mqtt.publish("/distance", String(rightOdometer.getDistance()));
+        gyro.update();
+        mqtt.publish("/rotation", String(gyro.getHeading()));
         mqtt.loop();
     }
+}
+
+void goForward(double distance) {
+  double currentDistance;
+  double startDistance = ((rightOdometer.getDistance() + leftOdometer.getDistance()) / 2);
+  do {
+    currentDistance = ((rightOdometer.getDistance() + leftOdometer.getDistance()) / 2);
+    car.setSpeed(manualSpeed);
+    car.setAngle(0);
+  } while(currentDistance < startDistance + distance);
+  car.setSpeed(0);
+  car.setAngle(0);
+}
+
+void waitStop(){
+  car.setSpeed(0);
+  rightMotor.setSpeed(0);
+  leftMotor.setSpeed(0);
+
+
+  while((leftOdometer.getSpeed() > 0.010) || (rightOdometer.getSpeed() > 0.010)){
+    delay(10); 
+  }
+  
+}
+
+
+void rotateTo0(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    
+
+
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * manualSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * -manualSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while(gyroCurrent != 180);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void rotateTo45(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    
+
+
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * manualSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * -manualSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while(gyroCurrent != 135);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void rotateTo90(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    
+
+
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * manualSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * -manualSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while(gyroCurrent != 90);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void rotateTo135(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    
+
+
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * manualSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * -manualSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while(gyroCurrent != 45);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void rotateTo180(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    
+
+
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * manualSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * -manualSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while(gyroCurrent != 0 || gyroCurrent != 360);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void rotateTo225(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    
+
+
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * manualSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * -manualSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while(gyroCurrent != 315);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void rotateTo270(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    
+
+
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * manualSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * -manualSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while(gyroCurrent != 270);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+}
+
+void rotateTo315(int startGyro){
+    gyro.update();
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
+    
+
+
+    int gyroCurrent = startGyro;
+
+    do {
+      rightMotor.setSpeed(SPEED_REDUCE * manualSpeed);
+      leftMotor.setSpeed(SPEED_REDUCE * -manualSpeed);
+      gyro.update();
+      gyroCurrent = gyro.getHeading();
+    } while(gyroCurrent != 225);
+    rightMotor.setSpeed(0);
+    leftMotor.setSpeed(0);
 }
