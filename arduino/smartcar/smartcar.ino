d4f16d1aebb25d718ed3f0a53f3cb7d3b52f072e
esjolund <72734963+esjolund@users.noreply.github.com>
2021-05-11 14:49:06 +0200

    Testing PQueue for commands via IOS app

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,85 +1,32 @@
-#include <MQTT.h>
-#include <WiFi.h>
-#include <Smartcar.h>
-
-MQTTClient mqtt;
-
-const int lDegrees = -90; // degrees to turn left
-const int rDegrees = 90;  // degrees to turn right
-int manualSpeed = 70;
-bool goingForward = true; //true is forward, false is backward
-
-ArduinoRuntime arduinoRuntime;
-BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
-BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
-DifferentialControl control(leftMotor, rightMotor);
-
-
-SimpleCar car(control);
+#include <Array.h>
 
 void setup() {
-    Serial.begin(9600);
-    //Uncomment line below
-    //mqtt.begin("localhost", 1883, WiFi);
-    // Will connect to localhost port 1883 be default
-    if (mqtt.connect("arduino", "public", "public")) {
-        mqtt.subscribe("/", 2); // Subscribing to topic "/"
-        mqtt.subscribe("speed", 2); //Subscribing to topic "speed"
-        mqtt.onMessage(+[](String topic, String message) {
-          Serial.println(message);
-            if(topic == "/"){
-              if(message == "w") {
-                car.setSpeed(manualSpeed);
-                car.setAngle(0);
-                Serial.println("XXX");
-              }
-              else if(message == "stop") {
-                car.setSpeed(0);
-                car.setAngle(0);
-              }
-              else if(message == "s") {
-                car.setSpeed(-manualSpeed);
-                car.setAngle(0);
-              }
-              else if(message == "d") {
-                car.setSpeed(manualSpeed);
-                car.setAngle(rDegrees);
-              }
-              else if(message == "a") {
-                car.setSpeed(manualSpeed);
-                car.setAngle(lDegrees);
-              }
-              else {
-                car.setSpeed(0);
-                car.setAngle(0);
-              }
-            }
-            if(topic == "speed"){
-              car.setSpeed(message.toInt());
-              car.setAngle(0);
-            }
-    }
-            
-            
-        );}
+  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
-
-// Start server in terminal with: /usr/local/sbin/mosquitto
-
-// Start sketch
-
-// In terminal write: mosquitto_pub -h 127.0.0.1 -t / -m "MESSAGE HERE"
-// and it will show up in serial.
-
-// To subscribe in terminal write: mosquitto_sub -h 127.0.0.1 -t /
-
-
-
 void loop() {
-    if (mqtt.connected()) {
-        //mqtt.publish("/", "Hello");
-        //mqtt.publish("speed", manualSpeed);
-        mqtt.loop();
+  // put your main code here, to run repeatedly:
+  char command = Serial.read();
+  priorityQueue(command);
+}
+
+void priorityQueue(char command) {
+  const int ARRAY_MAX = 100;
+  boolean previousCommand;
+
+  Array<String, ARRAY_MAX> array;
+  unsigned int arr_start = 0;
+
+  while (array.size() > 0) {
+    array[arr_start] = command;
+    if (previousCommand) {
+      if (Serial.println(command)) {
+        previousCommand = true;
+        arr_start ++;
+      } else {
+        Serial.print("Something is wrong with the queue.");
+      }
     }
+  }
 }
